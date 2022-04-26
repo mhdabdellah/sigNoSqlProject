a82ed0db55a884000ed8f8764db3df924abde1f5
mhdabdellah <86131274+mhdabdellah@users.noreply.github.com>
2022-04-26 11:39:49 +0200

    customise admin panel

--- distance_proj/settings.py
+++ distance_proj/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'measurements.apps.SuiteConfig',
+    'measurements.apps.SuiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- measurements/apps.py
+++ measurements/apps.py
@@ -1,12 +1,14 @@
 from django.apps import AppConfig
-# pip install https://github.com/darklow/django-suit/tarball/v2
-# from suit.apps import DjangoSuiteConfig
+# for install django-suit
+# https://django-suit.readthedocs.io/en/v2/install.html
+from suit.apps import DjangoSuitConfig
 
 
 class MeasurementsConfig(AppConfig):
     name = 'measurements'
     verbose_name = 'Measurement between 2 locations'
 
-# class SuiteConfig(DjangoSuiteConfig):
-#     layout = "horizontal" 
-    # vous pouvez utiliser layout = "vertical"
+class SuiteConfig(DjangoSuitConfig):
+    # layout = "horizontal" 
+    # vous pouvez utiliser 
+    layout = "vertical"
